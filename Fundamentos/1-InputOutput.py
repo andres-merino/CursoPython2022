cb316c40116cfb22906fe89f915eccf5b55f6a0b
Andres Merino <aemerinot@gmail.com>
2022-02-15 18:27:17 -0500

    feat: Clase 9

--- Fundamentos/1-InputOutput.py
+++ Fundamentos/1-InputOutput.py
@@ -1,8 +1,23 @@
-print("Formulario")
-nombre = input("Nombre: ")
-apellido = input("Apellido: ")
-edad = input("Edad: ")
+# Función entrada y función de salida
+# Va a buscar las palabras reservas
 
-print("El nombre ingresado es:", nombre)
-print("El apellido ingresado es:", apellido)
-print("El edad ingresada es:", edad)
+# Argumento a la función, '' -> string
+print('Formulario')
+print('Nombre: ', end='')
+# Input -> entrada datos
+nombre = input()
+print('Apellido: ', end='')
+apellido = input()
+print('Edad: ', end='')
+edad = input()
+
+# Primer reto curso, implicita print
+ciudad = input('Ingresa tu ciudad: ')
+
+print('El nombre ingresado es: ', nombre)
+print('El apellido ingresado es: ', apellido)
+print('Tu edad ingresada es: ', edad)
+print('Tu ciudad ingresada es: ', ciudad)
+
+
+# Python es un lenguaje 100% orientado a objetos
--- Fundamentos/2-Variables.py
+++ Fundamentos/2-Variables.py
@@ -1,9 +1,20 @@
+# Variables
+# No deben caracteres especiales, no deben empezar con mayusculas, no debe
+# palabra reservada
+
+# Tipos de Variables en python
+# Python es un lenguaje de tipado débil
+# Inicialización de las variables
+# 4. El tipo de dato de las variables viene dado por su inicialización
+# cast -> casting de variables
+
 edad = 22
-print(type(edad))
+nombre = 'Anderson'
+# La declaración de os flotantes es con un punto
+ira = 37.2
+mayorEdad = True
 
-edad = 22.2
-print(type(edad))
-
-print(type(edad))
-
-# Bien
+print('El tipo de dato de la variable edad es: ', type(edad))
+print('El tipo de dato de la variable nombre es: ', type(nombre))
+print('El tipo de dato de la variable IRA es: ', type(ira))
+print('El tipo de dato de la variable mayor de edad es: ', type(mayorEdad))
